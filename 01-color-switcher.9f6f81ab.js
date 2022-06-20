29be731a850807e3c3ab967425a12c3249094bb5
dimaniagara <86453739+dimaniagara@users.noreply.github.com>
2022-06-20 01:59:05 +0000

    Deploying to gh-pages from @ dimaniagara/goit-js-hw-09@548a28c75ee20d42bdd0a1381d31f9bd1e4f15ee 🚀

---
+++ 01-color-switcher.9f6f81ab.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){timerId=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16));document.body.style.background=t}),1e3),t.setAttribute("disabled","disabled")})),e.addEventListener("click",(function(){clearInterval(timerId),t.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.9f6f81ab.js.map
---
+++ 01-color-switcher.d276e518.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");t.addEventListener("click",(()=>{timerId=setInterval((()=>{const t=`#${Math.floor(16777215*Math.random()).toString(16)}`;document.body.style.background=t}),1e3),t.setAttribute("disabled","disabled")})),e.addEventListener("click",(()=>{clearInterval(timerId),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.d276e518.js.map
